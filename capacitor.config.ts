e21311ee9b809160cbf81be0f9381ddd9623faa3
himanushi <himanushi@users.noreply.github.com>
2022-10-11 21:07:30 +0900

    add ios files

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "net.himakan.app.vgm2",
+  appName: "video-game-music",
+  bundledWebRuntime: false,
+  webDir: "src",
+};
+
+export default config;
