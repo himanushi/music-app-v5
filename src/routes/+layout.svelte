b577b2f60a78ee112ec1a75477818bccd9593320
himanushi <himanushi@users.noreply.github.com>
2022-10-17 17:46:29 +0900

    確認しやすいようにした

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
   import Content from "./content.svelte";
   import Footer from "./footer.svelte";
   import Initializer from "~/components/initializer/initializer.svelte";
+  import { playerService } from "~/machines/apple-music-player-machine";
+
+  $: window.player = $playerService;
 </script>
 
 <ChangePage />
