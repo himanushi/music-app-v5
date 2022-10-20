ac95478853855d88104a0631faac322dcb9314ec
himanushi <himanushi@users.noreply.github.com>
2022-10-21 07:18:09 +0900

    fix

--- src/routes/library/albums/[id]/+page.svelte
+++ src/routes/library/albums/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { CapacitorMusicKit, type GetLibraryAlbumResult } from "capacitor-plugin-musickit";
+  import LibraryArtistItem from "../../artists/library-artist-item.svelte";
   import type { PageData } from "./$types";
   import CenterItem from "~/components/center-item.svelte";
   import ItemDivider from "~/components/item-divider/item-divider.svelte";
@@ -8,7 +9,7 @@
   import { convertImageUrl } from "~/lib/convertImageUrl";
   import { toTrackItem } from "~/lib/toTrackItem";
   import { accountService } from "~/machines/apple-music-account-machine";
-  import Item from "~/routes/library/tracks/item.svelte";
+  import LibraryTrackItem from "~/routes/library/tracks/library-track-item.svelte";
 
   export let data: PageData;
   let result: GetLibraryAlbumResult | undefined;
@@ -16,9 +17,8 @@
   const getItem = async () => {
     result = await CapacitorMusicKit.getLibraryAlbum({
       id: data.id,
-      include: ["tracks"],
+      include: ["tracks", "artists"],
     });
-    console.log({ result });
   };
 
   $: if ($accountService && $accountService.matches("authorized")) {
@@ -54,12 +54,19 @@
     </ion-item>
     <ItemDivider title="Tracks" />
     <VirtualScroll itemHeight={46} items={result.tracks} let:index let:item>
-      <Item
+      <LibraryTrackItem
         ids={result.tracks.map((track) => track.id)}
         {index}
         item={toTrackItem(item)}
         viewImage={false}
       />
+    </VirtualScroll>
+  {/if}
+
+  {#if result?.artists}
+    <ItemDivider title="Artists" />
+    <VirtualScroll itemHeight={46} items={result.artists} let:item>
+      <LibraryArtistItem {item} />
     </VirtualScroll>
   {/if}
 </ion-item-group>
