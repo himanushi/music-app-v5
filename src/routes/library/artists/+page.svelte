91c93f5636061ccd9a52f9a13e33bd494f84d9e8
himanushi <himanushi@users.noreply.github.com>
2022-10-21 07:18:14 +0900

    fix

--- src/routes/library/artists/+page.svelte
+++ src/routes/library/artists/+page.svelte
@@ -1,1 +1,29 @@
-アーティスト
+<script lang="ts">
+  import { CapacitorMusicKit, type GetLibraryArtistsResult } from "capacitor-plugin-musickit";
+  import LibraryAlbumItem from "./library-artist-item.svelte";
+  import ItemDivider from "~/components/item-divider/item-divider.svelte";
+  import VirtualScroll from "~/components/virtual-scroll.svelte";
+  import { isAuthorized } from "~/store/isAuthorized";
+
+  let result: GetLibraryArtistsResult | undefined;
+
+  const getItems = async () => {
+    result = await CapacitorMusicKit.getLibraryArtists({
+      limit: 100,
+      offset: 0,
+    });
+  };
+
+  $: if ($isAuthorized) {
+    getItems();
+  }
+</script>
+
+<ion-list>
+  <ItemDivider title="Library Artists" />
+  {#if result}
+    <VirtualScroll itemHeight={46} items={result.artists} let:item>
+      <LibraryAlbumItem {item} />
+    </VirtualScroll>
+  {/if}
+</ion-list>
