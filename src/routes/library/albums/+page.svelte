df304c2e6b38e884599deb16c08921a0be31353a
himanushi <himanushi@users.noreply.github.com>
2022-10-21 07:18:04 +0900

    fix

--- src/routes/library/albums/+page.svelte
+++ src/routes/library/albums/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import { CapacitorMusicKit, type GetLibraryAlbumsResult } from "capacitor-plugin-musickit";
-  import { goto } from "$app/navigation";
+  import LibraryAlbumItem from "./library-album-item.svelte";
   import ItemDivider from "~/components/item-divider/item-divider.svelte";
-  import SquareImage from "~/components/square-image.svelte";
   import VirtualScroll from "~/components/virtual-scroll.svelte";
-  import { convertImageUrl } from "~/lib/convertImageUrl";
   import { isAuthorized } from "~/store/isAuthorized";
 
   let result: GetLibraryAlbumsResult | undefined;
@@ -25,20 +23,7 @@
   <ItemDivider title="Library Albums" />
   {#if result}
     <VirtualScroll items={result.albums} let:item>
-      <ion-item button detail={false} on:click={() => goto(`/library/albums/${item.id}`)}>
-        <ion-thumbnail slot="start">
-          <SquareImage
-            src={convertImageUrl({
-              px: 300,
-              url: item.artworkUrl,
-            })}
-          />
-        </ion-thumbnail>
-        <ion-label>{item.name}</ion-label>
-        <ion-buttons slot="end">
-          <ion-icon name="heart-outline" slot="icon-only" />
-        </ion-buttons>
-      </ion-item>
+      <LibraryAlbumItem {item} />
     </VirtualScroll>
   {/if}
 </ion-list>
