c93c4708e3dc48c764697adfec0036f28368281c
himanushi <himanushi@users.noreply.github.com>
2022-10-27 18:34:20 +0900

    add skeleton-items

---
+++ src/components/skeleton-items.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let length = 1;
+</script>
+
+{#each [...Array(length)] as _}
+  <ion-item>
+    <ion-label>
+      <ion-skeleton-text />
+    </ion-label>
+  </ion-item>
+{/each}
