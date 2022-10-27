08789f293c06d1a7ec40709b690195ff96dcb486
himanushi <himanushi@users.noreply.github.com>
2022-10-27 18:34:29 +0900

    add error

---
+++ src/lib/error.ts
@@ -0,0 +1,27 @@
+import type { ApolloError } from "@apollo/client/core";
+import { mergeWith, camelCase } from "lodash";
+
+const customizer = (objValue: any, srcValue: any) => {
+  if (Array.isArray(objValue)) {
+    return objValue.concat(srcValue);
+  }
+  return undefined;
+};
+
+export const errorMessages = (error: ApolloError): Record<string, string[]> => {
+  const labels = error.graphQLErrors.map((err) => {
+    let path: string;
+
+    if (err.extensions?.path) {
+      path = camelCase(err.extensions?.path as string);
+    } else {
+      path = "_";
+    }
+
+    const label: Record<string, string[]> = {};
+    label[path] = [err.message];
+    return label;
+  });
+
+  return labels.reduce((labelSum, label) => mergeWith(labelSum, label, customizer));
+};
