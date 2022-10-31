8af919f4645ce5b64df512835cbcf7e8630d5ce2
himanushi <himanushi@users.noreply.github.com>
2022-10-31 17:51:04 +0900

    不要な箇所を削除

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,6 @@
   import Content from "./content.svelte";
   import Footer from "./footer.svelte";
   import Initializer from "~/components/initializer/initializer.svelte";
-  import { playerService } from "~/machines/apple-music-player-machine";
-
-  $: window.player = $playerService;
 </script>
 
 <ChangePage />
