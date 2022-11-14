dde9ab439711c33216749d7c6cb829232cdf9f5e
himanushi <himanushi@users.noreply.github.com>
2022-11-15 06:22:48 +0900

    initialize 方法変更

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
   import "~/theme/variables.css";
   import "~/theme/custom.css";
+  import { initialize } from "@ionic/core";
+  import { onMount } from "svelte";
   import ChangePage from "./change-page.svelte";
   import Content from "./content.svelte";
   import Footer from "./footer.svelte";
   import Initializer from "~/components/initializer/initializer.svelte";
+
+  onMount(() => {
+    initialize({
+      animated: true,
+      mode: "ios",
+    });
+  });
 </script>
 
 <ChangePage />
