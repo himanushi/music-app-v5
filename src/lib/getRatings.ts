10ad2eeed1edff0a141547f65f3e40d0f28a2269
himanushi <himanushi@users.noreply.github.com>
2022-11-16 06:58:03 +0900

    add file

---
+++ src/lib/getRatings.ts
@@ -0,0 +1,37 @@
+import { CapacitorMusicKit } from "capacitor-plugin-musickit";
+
+export const getRatings = async (ids: string[]) => {
+  if (ids.length === 0) {
+    return [];
+  }
+
+  let ratings: MusicKit.Ratings[] = [];
+
+  const SongIds = ids.filter((id) => !id.startsWith("i."));
+  if (SongIds.length > 0) {
+    ratings = [
+      ...ratings,
+      ...(
+        await CapacitorMusicKit.getRatings({
+          ids: SongIds,
+          type: "songs",
+        })
+      ).data,
+    ];
+  }
+
+  const librarySongIds = ids.filter((id) => id.startsWith("i."));
+  if (librarySongIds.length > 0) {
+    ratings = [
+      ...ratings,
+      ...(
+        await CapacitorMusicKit.getRatings({
+          ids: librarySongIds,
+          type: "library-songs",
+        })
+      ).data,
+    ];
+  }
+
+  return ratings;
+};
