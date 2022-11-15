05c4776ca1af04d6af9383570de0d78346d6c17d
himanushi <himanushi@users.noreply.github.com>
2022-11-16 06:57:19 +0900

    add id

--- src/lib/toTrackItem.ts
+++ src/lib/toTrackItem.ts
@@ -1,6 +1,7 @@
 import type { TrackObject } from "~/graphql/types";
 
 export type TrackItem = {
+  id: string;
   artworkUrl?: string;
   index?: number;
   name: string;
@@ -10,12 +11,14 @@ export const toTrackItem = (track: MusicKit.LibrarySongs | TrackObject): TrackIt
   if ("attributes" in track) {
     return {
       artworkUrl: track.attributes.artwork?.url,
+      id: track.id,
       index: track.attributes.trackNumber,
       name: track.attributes.name,
     };
   }
   return {
     artworkUrl: track.artworkM.url,
+    id: track.appleMusicId,
     index: track.trackNumber,
     name: track.name,
   };
