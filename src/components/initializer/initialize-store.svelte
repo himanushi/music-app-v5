138bbc2c4a37e09f049da55f8158b482827b84c8
himanushi <himanushi@users.noreply.github.com>
2022-11-23 09:25:00 +0900

    お気に入りデータを復元

---
+++ src/components/initializer/initialize-store.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { favorites, storeId, type FavoriteType } from "~/store/favorites";
+  import { store } from "~/store/store";
+
+  onMount(async () => {
+    favorites.remember((await store.get(storeId)) as FavoriteType);
+  });
+</script>
--- src/components/initializer/initializer.svelte
+++ src/components/initializer/initializer.svelte
@@ -2,10 +2,12 @@
   import { Capacitor } from "@capacitor/core";
   import AppleMusicIosAccount from "./apple-music-ios-account.svelte";
   import AppleMusicPlayer from "./apple-music-player.svelte";
+  import InitializeStore from "./initialize-store.svelte";
   import AppleMusicLibraryAlbums from "~/components/initializer/apple-music-library-albums.svelte";
   import AppleMusicWebAccount from "~/components/initializer/apple-music-web-account.svelte";
 </script>
 
+<InitializeStore />
 <AppleMusicPlayer />
 <AppleMusicLibraryAlbums />
 {#if Capacitor.getPlatform() === "web"}
