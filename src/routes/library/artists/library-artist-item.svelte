3185f8115e8c6567e7ac00de8f3bc01904d596ac
himanushi <himanushi@users.noreply.github.com>
2022-12-05 14:03:24 +0900

    ライブラリアーティストはお気に入りできないので削除

--- src/routes/library/artists/library-artist-item.svelte
+++ src/routes/library/artists/library-artist-item.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
-  import FavoriteButton from "~/components/favorite-button.svelte";
-  import { isFavorite } from "~/lib/isFavorite";
-  import { favorites } from "~/store/favorites";
 
   export let item: MusicKit.LibraryArtists;
 </script>
 
 <ion-item button detail={false} on:click={() => goto(`/library/artists/${item.id}`)}>
   <ion-label>{item.attributes.name}</ion-label>
-  <ion-buttons>
-    <FavoriteButton
-      id={item.id}
-      categoryType="artists"
-      favorite={isFavorite($favorites, item.id)}
-    />
-  </ion-buttons>
 </ion-item>
