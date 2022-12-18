7c274fbef7ab1deaa6d068b72c3d2648c5562b73
himanushi <himanushi@users.noreply.github.com>
2022-12-18 18:00:42 +0900

    fix

--- src/routes/library/albums/+page.svelte
+++ src/routes/library/albums/+page.svelte
@@ -2,26 +2,18 @@
   import type { PageData } from "./$types";
   import LibraryAlbumItem from "./library-album-item.svelte";
   import ItemDivider from "~/components/item-divider/item-divider.svelte";
-  import Refresher from "~/components/refresher.svelte";
   import VirtualScroll from "~/components/virtual-scroll.svelte";
   import { libraryAlbumsService } from "~/machines/apple-music-library-albums-machine";
 
   export let data: PageData;
 
-  $: albums = $libraryAlbumsService.context.filteredAlbums;
-
-  let loaded = false;
-  const refresh = () => {
-    libraryAlbumsService.send("RESET");
-    loaded = true;
-  };
+  $: items = $libraryAlbumsService.context.filteredItems;
 </script>
 
-<Refresher {refresh} bind:loaded />
 <ion-list>
   <ItemDivider menu={data.menu} title="Library Albums" />
-  {#if albums}
-    <VirtualScroll items={albums} thumbnail let:item>
+  {#if items}
+    <VirtualScroll {items} thumbnail let:item>
       <LibraryAlbumItem {item} />
     </VirtualScroll>
   {/if}
