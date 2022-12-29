43ce882e01bafb0682e3da1513a5d64f0b8cb2cb
himanushi <himanushi@users.noreply.github.com>
2022-12-29 11:25:48 +0900

    track page

--- src/routes/library/tracks/+page.svelte
+++ src/routes/library/tracks/+page.svelte
@@ -1,1 +1,20 @@
-曲
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import LibraryItem from "./library-track-item.svelte";
+  import ItemDivider from "~/components/item-divider/item-divider.svelte";
+  import VirtualScroll from "~/components/virtual-scroll.svelte";
+  import { libraryAlbumsService } from "~/machines/apple-music-library-albums-machine";
+
+  export let data: PageData;
+
+  $: items = $libraryAlbumsService.context.filteredItems;
+</script>
+
+<ion-list>
+  <ItemDivider menu={data.menu} title="Library Albums" />
+  {#if items}
+    <VirtualScroll {items} thumbnail let:item>
+      <LibraryItem {item} />
+    </VirtualScroll>
+  {/if}
+</ion-list>
