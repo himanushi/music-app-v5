4985a2f97e6b9c7e64ae9b5482b17e99a6a1ad17
himanushi <himanushi@users.noreply.github.com>
2023-01-07 13:01:31 +0900

    fix

--- src/routes/library/tracks/+page.svelte
+++ src/routes/library/tracks/+page.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
-  import LibraryItem from "./library-track-item.svelte";
-  import ItemDivider from "~/components/item-divider/item-divider.svelte";
-  import VirtualScroll from "~/components/virtual-scroll.svelte";
-  import { libraryAlbumsService } from "~/machines/apple-music-library-albums-machine";
 </script>
 
 <ion-list />
